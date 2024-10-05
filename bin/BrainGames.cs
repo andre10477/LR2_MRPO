c9adb6f6efbd530cacd37e6acf7f8da01d2f4a5e
Andrey <andreyzhiryakov@mail.ru>
2024-10-05 14:20:57 +0300

    ++

---
+++ bin/BrainGames.cs
@@ -0,0 +1,32 @@
+﻿using LR2_MRPO.src.games;
+
+namespace LR2_MRPO.bin
+{
+    internal class BrainGames
+    {
+        static void Main(string[] args)
+        {
+            // Вызов приветствия и ввода имени
+            string userName = CLI.GreetUser();
+            // Выбор игры
+            Console.WriteLine("Choose game:\n" +
+                "1. Smallest common multiple\n" +
+                "2. Geometric progression\n");
+
+            //Ввод игры
+            int game = Convert.ToInt32(Console.ReadLine());
+
+            //Выбор игры, условие
+            if (game == 1)
+            {
+                CSM csm = new();
+                csm.Main(userName);  // Передаем имя пользователя в игру
+            }
+            else if (game == 2)
+            {
+                GeomProg gp = new();
+                gp.Main(userName);  // Передаем имя пользователя в игру
+            }
+        }
+    }
+}
